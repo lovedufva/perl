40997aa403defc59f4e7b3ac0ccf23ff66eed117
Love Dufva <love@lovedufva.se>
2014-12-13 23:19:26 +0100

    Create BusTime.pl
    I'm awful at xml parsing.

---
+++ BusTime.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+# use module
+use XML::Simple;
+use Data::Dumper;
+use LWP::Simple;
+use URI;
+
+# create object
+my $parser      = new XML::Simple;
+my $url         = URI->new( 'http://api.sl.se/api2/realtimedepartures.xml?' );
+$url->query_form(
+        'key'           => '9e99975c83864b6f8dab6ab4baddaba8',
+        'siteid'        => '1142',
+        'timewindow'    => '60',
+);
+my $file        = "sl.xml";
+
+#get response
+my $response    = getstore($url, $file);
+my $doc         = $parser->XMLin($file);
+
+#print Dumper($doc);
+# read XML file
+#/ResponseOfDepartures/ResponseData/Buses/Bus/"buss från"..StopAreaName.."Till"..Destination.."Avgår"..DisplayTime
+#die;
+foreach $e (@{$doc->{ResponseData}->{Buses}->{Bus}})
+{
+        print "Buss från ", $e->{StopAreaName}, " mot ", $e->{Destination}, " avgår om ", $e->{DisplayTime}, ".", "\n", "\n";
+}
